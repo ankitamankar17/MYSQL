68e3835fca4ec84516834bdbea0a4ac1e37771a1
ankitamankar17 <105000616+ankitamankar17@users.noreply.github.com>
2022-11-16 21:37:06 +0530

    Add files via upload

---
+++ assesment.sql
@@ -0,0 +1,16 @@
+create  database Assessment;
+
+create  table Countries(
+country_id int,
+ country_name varchar(255),
+ region_id int 
+ );
+ 
+ insert into Countries(country_id,country_name,region_id)
+  values('1' , 'Italy', '12');
+  
+  insert into Countries(country_id,country_name,region_id)
+  values('2' , 'India', '13');
+  
+  insert into Countries(country_id,country_name,region_id)
+  values('3' , 'China', '14');
